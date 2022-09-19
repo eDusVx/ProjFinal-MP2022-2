84d6d71bf770cb9e20fd84144bfdef38dcbdd708
Gabriel Farago <gabrielfaragounb@gmail.com>
2022-09-19 14:19:26 -0300

    Código Documentado _Gabriel

--- webpage/models.py
+++ webpage/models.py
@@ -14,6 +14,16 @@ from django.contrib.auth.models import User
 CHOICES = [("Pedido","Pedido"), ("Fazendo","Fazendo"), ("Pronto","Pronto")]
 TIPO_PRATO = [("Entrada","Entrada"),("Executivo","Executivo"),("Carne","Carne"),("Peixe","Peixe"),("Frango","Frango"),("Massa","Massa"),("Vegano/Vegetariano","Vegano/Vegetariano"),("Bebidas","Bebidas"),("Sobremesa","Sobremesa")]
 class Garcon(models.Model):
+    '''
+    Representa um garçom do restaurante
+
+    :ATRIBUTOS:
+        :user: Chave estrangeira referente ao usuário go garçom
+        :nome_completo: Nome completo do garçom
+ 
+    :RETORNA:
+        Nome completo em maiúsculo
+    '''
     user = models.ForeignKey(User, on_delete=models.CASCADE,default = 1)
     nome_completo = models.CharField(max_length=350)
     email = models.CharField(unique=True,max_length=150,default="default@email.com")
@@ -22,6 +32,17 @@ class Garcon(models.Model):
         return self.nome_completo.upper()
 
 class Mesa(models.Model):
+    '''
+    Representa uma mesa do restaurante.
+
+    :ATRIBUTOS:
+        :numero: Número da mesa
+        :quantidade: Número de pessoas na mesa
+        :garcon_responsavel: Chave estrangeira referente ao garçom responsável pela mesa 
+
+    :RETORNA:
+        Numero da mesa
+    '''
     numero = models.IntegerField(max_length=3)
     quantidade = models.IntegerField(max_length=2, blank=True, null=True, default=0)
     garcon_responsavel = models.ForeignKey(Garcon, on_delete=models.CASCADE, blank=True, null=True, default=None)
@@ -29,6 +50,25 @@ class Mesa(models.Model):
         return str(self.numero)
 
 class Ingrediente(models.Model):
+    '''
+    Representa um ingrediente com suas respectivas informações nutricionais.
+    
+    :ATRIBUTOS:
+        :nome: Nome do ingrediente
+        :quantidade: Quantidade da porção do ingrediente
+        :valor_energetico: Quantidade de valor energético presente na porção do ingrediente
+        :carboidratos: Quantidade de carboídratos presentes na porção do ingrediente
+        :proteinas: Quantidade de proteínas presentes na porção do ingrediente
+        :gorduras: Quantidade de gorduras presentes na porção do ingrediente
+        :fibra_alimentar: Quantidade de fibra alimentar presente na porção do ingrediente
+        :sodio: Quantidade de sódio presente na porção do ingrediente
+        :acucares: Quantidade de açucares presentes na porção do ingrediente
+        :colesterol: Quantidade de colesterol presente na porção do ingrediente   
+    
+    :RETORNA:
+        Nome do ingrediente em maiúsculo
+    
+    '''
     nome = models.CharField(max_length=350)
     quantidade = models.CharField(max_length=1000,default="100g")
     valor_energetico = models.FloatField(max_length=100,default=0)
@@ -43,6 +83,20 @@ class Ingrediente(models.Model):
         return self.nome.upper()
     
 class Prato(models.Model):
+    '''
+    Representa um prato do restaurante
+
+    :ATRIBUTOS:
+        :nome: Nome do prato
+        :foto: Foto do prato
+        :preco: Preço do prato
+        :tipo: Tipo do prato
+        :ingredientes: Chave da tabela intermediária para relação muitos para muitos dos ingredientes do prato
+        :destaque: Especifica se o prato está em destaque
+
+    :RETORNA:
+        Nome do prato em maiúsculo
+    '''
     nome = models.CharField(max_length=350)
     foto = models.ImageField(upload_to ='Images')
     preco = MoneyField(max_digits=14, decimal_places=2, default_currency='BRL')
@@ -53,6 +107,17 @@ class Prato(models.Model):
         return self.nome.upper()
     
 class Pedido(models.Model):
+    '''
+    Representa um pedido de uma mesa
+
+    :ATRIBUTOS:
+        :numero: Número do pedido
+        :pratos: Pratos do pedido
+        :garcon: Garçom responsável pelo pedido
+        :mesa: Mesa do pedido
+        :prato: Chave da tabela intermediária para relação muitos para muitos dos pratos do pedido
+        :status: Status do pedido
+    '''
     numero = models.IntegerField(max_length=1000)
     pratos = models.TextField()
     garcon = models.ForeignKey(Garcon, on_delete=models.CASCADE, default=0)
@@ -61,16 +126,43 @@ class Pedido(models.Model):
     status = models.CharField(max_length=100, choices=CHOICES, default="Pedido")
 
 class pedido_has_prato(models.Model):
+    '''
+    Representa tabela intermediária dos pratos do pedido
+
+    :ATRIBUTOS:
+        :prato: Chave estrangeira referente ao prato
+        :pedido: Chave estrangeira referente ao pedido
+        :quantidade: Quantidade do prato no pedido
+    '''
     prato = models.ForeignKey(Prato, on_delete=models.CASCADE)
     pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
     quantidade = models.IntegerField(max_length=3, default=0)
 
 class prato_has_ingrediente(models.Model):
+    '''
+    Representa tabela intermediária dos ingredientes do prato
+
+    :ATRIBUTOS:
+        :prato: Chave estrangeira referente ao prato
+        :ingrediente: Chave estrangeira referente ao ingrediente
+        :quantidade: Quantidade do ingrediente no prato
+    '''
     prato = models.ForeignKey(Prato, on_delete=models.CASCADE)
     ingrediente = models.ForeignKey(Ingrediente, on_delete=models.CASCADE)
     quantidade = models.FloatField(max_length=3)
 
 class Estoque(models.Model):
+    '''
+    Representa o estoque de ingredientes do restaurante
+
+    :ATRIBUTOS:
+        :ingrediente: Chave estrangeira refente ao ingrediente a ser estocado
+        :quantidade: Quantidade do ingrediente a ser estocado
+        :validade: Validade do ingrediente a ser estocado
+    
+    :RETORNA:
+        Nome do ingrediente
+    '''
     ingrediente = models.ForeignKey(Ingrediente, on_delete=models.CASCADE)
     quantidade = models.IntegerField(max_length=3)
     validade = models.DateField(default=datetime.now())
--- webpage/tests/test_views.py
+++ webpage/tests/test_views.py
@@ -12,12 +12,23 @@ from webpage.models import Garcon, Mesa, Pedido
 
 ])
 def test_views_sem_login(client, param):
+    """Teste para testar a tela inicial sem login
+	
+	:RECEBE:
+	:client: Simulacao do cliente na url testada
+	"""
     temp_url = urls.reverse(param)
     resp = client.get(temp_url)
     assert resp.status_code == 200
 
 @pytest.mark.django_db
 def test_loginPage(client, create_test_user, user_data):
+	"""Teste da pagina de login
+
+	:RECEBE:
+	:client: Simulacao do cliente na url testada
+	:user_data: Dados de usuario cadastrado
+	"""
 	login_url = urls.reverse('login')
 	resp = client.post(login_url, data=user_data)
 	assert resp.status_code == 302
@@ -26,6 +37,12 @@ def test_loginPage(client, create_test_user, user_data):
 
 @pytest.mark.django_db
 def test_logoutUser(client, authenticated_user):
+	"""Teste para logout de usuario
+
+	:RECEBE:
+	:client: Simulacao do cliente na url testada
+	:authenticated_user: Usuario autenticado no sistema
+	"""
 	logout_url = urls.reverse('logout')
 	resp = client.get(logout_url)
 	assert resp.status_code == 302
@@ -33,12 +50,22 @@ def test_logoutUser(client, authenticated_user):
 
 @pytest.mark.django_db
 def test_Home(client,user_data):
+	"""Teste da pagina Home
+	
+	:RECEBE:
+	:client: Simulacao do cliente na url testada
+	:user_data: Dados de usuario cadastrado
+	"""
 	login_url = urls.reverse('home')
 	resp = client.post(login_url, data=user_data)
 	assert resp.status_code == 302
 
 @pytest.mark.django_db
 def test_nuemro_mesa(client):
+    """Teste para verificar o numero da mesa
+	
+	:RECEBE:
+	:client: Simulacao do cliente na url testada"""
     mesa = Mesa.objects.create(numero=1, quantidade = 3)
     logout_url = urls.reverse('customer', args=[mesa.pk])
     resp = client.get(logout_url)
@@ -46,6 +73,10 @@ def test_nuemro_mesa(client):
 
 @pytest.mark.django_db
 def test_atualizar_pedido(client):
+    """Teste para atualizar pedido
+	
+	:RECEBE:
+	:client: Simulacao do cliente na url testada"""
     pedido = Pedido.objects.create(numero =1,mesa_id = 1,garcon_id = 1)
     temp_url = urls.reverse('update_order', args=[pedido.pk])
     resp = client.get(temp_url)
@@ -53,6 +84,10 @@ def test_atualizar_pedido(client):
 
 @pytest.mark.django_db
 def test_deletar_pedido(client):
+	"""Teste para deletar pedido
+	
+	:RECEBE:
+	:client: Simulacao do cliente na url testada"""
 	pedido = Pedido.objects.create(numero =1,mesa_id = 1,garcon_id = 1)
 	temp_url = urls.reverse('delete_order', args=[pedido.pk])
 	resp = client.get(temp_url)
@@ -60,8 +95,12 @@ def test_deletar_pedido(client):
 
 @pytest.mark.django_db
 def test_registrar_pedido(client):
+	"""Teste para registrar pedido
+	
+	:RECEBE:
+	:client: Simulacao do cliente na url testada"""
 	pedido = Pedido.objects.create(numero =1,mesa_id = 1,garcon_id = 1)
-	temp_url = urls.reverse('registrar_pedido')
+	temp_url = urls.reverse('registrar_pedido', args=[1])
 	resp = client.get(temp_url)
 	assert resp.status_code == 302
 
